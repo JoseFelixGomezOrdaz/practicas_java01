66ba729de61f62c6c7603af7ed471f10d9161d63
JoseFelixGomezOrdaz <37378259+JoseFelixGomezOrdaz@users.noreply.github.com>
2018-05-18 00:05:11 -0500

    Add files via upload

---
+++ DatosPrimitivos.java
@@ -0,0 +1,17 @@
+public class DatoPrimitivos {
+    public static void main(String[ ] jfgo) {
+    byte byte_min=-128;
+        System.out.println("byte min" + byte_min);
+    byte byte_max=127;
+        System.out.println("byte max" + byte_max);
+    short short_min= -32768;
+        System.out.println("short min" + short_min);
+    short short_max= 32767;
+        System.out.println("short max" + short_max );
+    int int_min=-2147483648;
+        System.out.println("int min" + int_min );
+    int int_max=2147483647;
+        System.out.println("int max" + int_max );
+    
+    }
+}
